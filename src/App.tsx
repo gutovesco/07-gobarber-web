9b93d124baa0bfc791b7b9aa5a9d297b039bfb04
gutovesco <guto.vesco@hotmail.com>
2020-04-28 12:52:26 -0300

    global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import GlobalStyle from './styles/global'
 
 const App: React.FC = () => (
-  <h1>ba</h1>
+  <>
+  <h1 style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>GoBarber Web</h1>
+  <GlobalStyle/>
+  </>
 )
 export default App;
