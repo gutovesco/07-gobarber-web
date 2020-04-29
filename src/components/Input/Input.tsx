1fdb561b1b1cabebdae360315c1bd7dafa8979c4
gutovesco <guto.vesco@hotmail.com>
2020-04-29 16:52:27 -0300

    unform add

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,17 +1,31 @@
-import React, {InputHTMLAttributes} from 'react'
+/* eslint-disable no-restricted-globals */
+import React, {InputHTMLAttributes, useEffect, useRef} from 'react'
 import {Container} from './styles'
 import {IconBaseProps} from 'react-icons'
+import {useField} from '@unform/core'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
     name: string;
     icon?: React.ComponentType<IconBaseProps>;
 }
 
-const Input: React.FC<InputProps> = ({icon: Icon, ...rest}) => (
+const Input: React.FC<InputProps> = ({name, icon: Icon, ...rest}) => {
+    const inputRef = useRef(null)
+    const {fieldName, error, registerField} = useField(name)
+
+    useEffect(() => {
+        registerField({
+            name:fieldName,
+            ref: inputRef.current,
+            path: 'value'
+        })
+    }, [fieldName, registerField])
+    return (
     <Container>
         { Icon && <Icon size={20}/>}
-        <input {...rest}></input>
+        <input ref={inputRef} {...rest}></input>
     </Container>
-)
+    )
+}
 
 export default Input
--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -6,29 +6,31 @@ import signUpBackground from '../../assets/sign-up-background.png'
 import Input from '../../components/Input/Input'
 import Button from '../../components/Button/Button'
 import {FiArrowLeft, FiMail, FiUser, FiLock} from 'react-icons/fi'
+import {Form} from '@unform/web'
 
-const SignUp: React.FC = () => (
-    <>
-    <div>
+const SignUp: React.FC = () => {
+
+    function handleSubmit( data: object): void {
+        console.log(data)
+    }
+    return (
         <Container>
         <Background><img style={{flex: 1, backgroundSize: 'cover'}} src={signUpBackground} alt="img"></img></Background>
             <Content>
                 <img style={{paddingTop: 200}} src={LogoImg} alt="logo"></img>
 
-                <form>
+                <Form onSubmit={handleSubmit}>
                     <h1>Faça seu Cadastro</h1>
                     <Input name="name" icon={FiUser} placeholder="Nome"></Input>
                     <Input name="email" icon={FiMail} placeholder="Email"></Input>
                     <Input name="password" icon={FiLock} placeholder="Senha" type="password"></Input>
                     <Button type="submit">Cadastrar</Button>
-                </form>
+                </Form>
 
                 <a href="login"><FiArrowLeft/>Voltar para logon</a>
             </Content>
         </Container>
-    </div>
-    </>
-  
-)
+    )
+}
 
 export default SignUp
